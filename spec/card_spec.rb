aee7b43917dfe7554b4d75deb12ccd6d9fe9007a
Greybeard82 <davidvicentemartins@gmail.com>
2020-05-17 15:50:44 +0200

    Learned about 'before', 'let!' and 'let' commands. Refactoring of test file using them

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,9 +8,20 @@ class Card
 end
 
 RSpec.describe Card do
+
+  #RSpec method that allows memoization in lines 22 and 23
+  let (:card) {Card.new('Ace', 'Spaces')}
+
+  # Method to generate a new card each time it's called
   # before do
   #   @card = Card.new('Ace','Spades')
   # end
+
+  it 'Has a rank and it can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
 
   def card
     card = Card.new("Ace", "Spades")
