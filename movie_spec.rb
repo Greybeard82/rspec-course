a2ab70565e8e1579056251be68d268a6acb98f1b
Greybeard82 <davidvicentemartins@gmail.com>
2020-12-15 12:25:15 +0100

    Explanation and practical use of doubles in rspec

---
+++ movie_spec.rb
@@ -0,0 +1,63 @@
+# lesson to create boilerplate for next lesson. Explain dependancy between classes and how that affects testing
+
+class Actor
+    def initialize(name)
+        @name = name
+    end
+
+    def ready?
+        sleep(3)
+        true
+    end
+
+    def act
+        "I love you baby"
+    end
+
+    def fall_off_ladder
+        'Call my agent. No way'
+    end
+
+    def light_on_fire
+        false
+    end
+end
+
+class Movie
+    attr_reader :actor
+
+    def initialize(actor)
+        @actor = actor
+    end
+
+    def start_shooting
+        if actor.ready?
+            actor.act
+            actor.fall_off_ladder
+            actor.light_on_fire
+        end
+    end
+end
+
+# actor = Actor.new('Charles Bronson')
+# movie = Movie.new(actor)
+
+# movie.start_shooting
+
+#In order to test the classes, we create a double.
+
+RSpec.describe Movie do
+    let(:stuntman) {double("Mr Danger", ready?: true, act: 'Any string at all', fall_off_ladder: 'Sure!', light_on_fire: true)}
+    subject {described_class.new(stuntman)}
+
+    describe 'start shooting method' do
+        it 'expects an actor to do 3 actions' do
+            expect(stuntman).to receive(:ready?)
+            expect(stuntman).to receive(:act)
+            expect(stuntman).to receive(:fall_off_ladder)
+            expect(stuntman).to receive(:light_on_fire)
+            subject.start_shooting
+           
+        end
+    end
+end
